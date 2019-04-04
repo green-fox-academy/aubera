aa554894ac3966cec3ae51a8e373c93efd10452a
aubera <andor.auber@gmail.com>
2019-04-04 17:00:11 +0200

    feat(i-wont-cheat-on-the-exams.ts) add code to write out this sentence 100 times

---
+++ week-01/day-4/i-wont-cheat-on-the-exams.ts
@@ -0,0 +1,9 @@
+
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+for (let i: number = 0; i < 100; i++) {
+  console.log((i + 1) + ': I won\'t cheat on the exam!');
+}
