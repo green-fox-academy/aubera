a98b08d50f4fe611194458d7bfe4a1140f7901ea
aubera <andor.auber@gmail.com>
2019-04-04 16:13:03 +0200

    feat(bmi.ts) add bmi calculator

---
+++ week-01/day-4/bmi.ts
@@ -0,0 +1,11 @@
+
+'use strict';
+
+let massInKg: number = 81.2;
+let heightInM: number = 1.78;
+
+// Print the Body mass index (BMI) based on these values
+
+let bmi: number = massInKg/(heightInM**2);
+
+console.log('Your BMI is: ' + Math.round(bmi*10)/10);
