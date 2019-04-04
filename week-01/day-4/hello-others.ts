7efb4ed78314ab0ba51e3ea3cd3aed8c951f6ace
aubera <andor.auber@gmail.com>
2019-04-04 15:57:29 +0200

    feat(hello-others.ts) add 3 lines of console.log to greet my classmates

---
+++ week-01/day-4/hello-others.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+// Greet 3 of your classmates with this program, in three separate lines like:
+//
+console.log('Hello, Pisti!')
+console.log('Hello, Pöli!')
+console.log('Hello, Alpi!')
