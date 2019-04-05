b5737dd6062efe73b7237fa415d8fced81bbba6d
aubera <andor.auber@gmail.com>
2019-04-05 12:29:34 +0200

    feat(substr.ts) add another function which doesn't use built-in .search function

--- week-01/day-4/substr.ts
+++ week-01/day-4/substr.ts
@@ -4,15 +4,29 @@
 //  Returns the starting index where the second one is starting in the first one
 //  Returns `-1` if the second string is not in the first one
 
-function substr(str: string, keyword: string) : number {
+function substr(str: string, keyword: string): number {
   let position: number = str.search(keyword);
   return position;
 }
 
+function substr2(str: string, keyword: string): number {
+  let lengthOfSearchString: number = str.length;
+  let lengthOfSearchKeyword: number = keyword.length;
+  let position: number = -1;
+
+  for (let i: number = 0; i < (lengthOfSearchString - lengthOfSearchKeyword); i++) {
+    if (str.slice(i, i + lengthOfSearchKeyword) === keyword) {
+      position = i;
+    }
+  }
+  return position;
+}
+
+
 //  Example
 
 // should print: `17`
-console.log(substr("this is what I'm searching in", "searching"));
+console.log(substr2("this is what I'm searching in", "searching"));
 
 // should print: `-1`
-console.log(substr("this is what I'm searching in", "not"));
+console.log(substr2("this is nwhat I'm searching in", "not"));
