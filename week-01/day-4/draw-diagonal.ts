18a9f93b682b7eb77e8c12d2d733f01349807bb5
aubera <andor.auber@gmail.com>
2019-04-05 10:50:50 +0200

    feat(draw-diagonal.ts) add code to draw a diagonal into a square

---
+++ week-01/day-4/draw-diagonal.ts
@@ -0,0 +1,35 @@
+'use strict';
+
+let lineCount: number = 6;
+let lineBase: string = '';
+let lineElement: string = '%';
+let emptyElement: string = ' ';
+let numberOfSpacesInLines: number = lineCount - 3;
+
+// Write a program that draws a
+// square like this:
+//
+// %%%%%%
+// %%   %
+// % %  %
+// %  % %
+// %   %%
+// %%%%%%
+//
+// The square should have as many lines as lineCount is
+
+function repeater(base: string, element: string, repeat: number) {
+  for (let i: number = 1; i <= repeat; i++) {
+    base += element;
+  }
+  return (base);
+}
+
+for (let i: number = 0; i < lineCount; i++){
+  if (i === 0 || i === lineCount - 1){
+    console.log(repeater(lineBase, lineElement, lineCount));
+  } else {
+    console.log('%' + repeater(lineBase, emptyElement, i - 1) + '%' + repeater(lineBase, emptyElement, numberOfSpacesInLines) + '%');
+    numberOfSpacesInLines--;
+  }
+}
