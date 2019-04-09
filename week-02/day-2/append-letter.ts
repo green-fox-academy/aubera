8b73515feeb94718a802bfc9db8d4f631b59bd6e
aubera <andor.auber@gmail.com>
2019-04-09 14:50:49 +0200

    feat(append-letter.ts) add function to append a letter to the end of each element in an array

---
+++ week-02/day-2/append-letter.ts
@@ -0,0 +1,19 @@
+'use strict';
+// Create a function called "appendA()" that adds "a" to every string in the far list.
+// The parameter should be a list.
+
+let far: string[] = ['bo', 'anacond', 'koal', 'pand', 'zebr'];
+
+function appendA(list: string[]): string[] {
+  let newList: string[] = [];
+  for (let i: number = 0; i < list.length; i++) {
+    newList.push(list[i].concat('a'));
+  }
+  return newList;
+}
+
+console.log(appendA(far));
+
+// The output should be: 'boa', 'anaconda', 'koala', 'panda', 'zebra'
+
+export = appendA;
