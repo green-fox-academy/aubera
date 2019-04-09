55f6f3118d973224a13476c2f188476b57f316b3
aubera <andor.auber@gmail.com>
2019-04-09 11:00:55 +0200

    feat(diagonal-matrix.ts) add function to make a scalable matrix dynamically, delete unnecessary lines from code

--- week-02/day-1/diagonal-matrix.ts
+++ week-02/day-1/diagonal-matrix.ts
@@ -13,18 +13,20 @@
 // * size should depend on a variable
 // ** Relax, a matrix is just like an array
 
-let size: number = 4;
-let matrix: number[][] = [];
-let line: number[];
-let diagonal: number = size - 1;
 
-for (let i: number = 0; i < size; i++) {
-  matrix[i] = [];
-  for (let j: number = 0; j < size; j++) {
-    matrix[i][j] = 0;
+
+function diagonalMatrix(size: number) {
+  let matrix: number[][] = [];
+  let diagonal: number = size - 1;
+  for (let i: number = 0; i < size; i++) {
+    matrix[i] = [];
+    for (let j: number = 0; j < size; j++) {
+      matrix[i][j] = 0;
+    }
+    matrix[i][diagonal] = 1;
+    diagonal--;
   }
-  matrix[i][diagonal] = 1;
-  diagonal--;
+  console.log(matrix);
 }
 
-console.log(matrix);
+diagonalMatrix(25);
