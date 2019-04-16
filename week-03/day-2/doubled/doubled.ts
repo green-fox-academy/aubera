bd0b4f2108704b34f57b58faa5f4b435461040ed
aubera <andor.auber@gmail.com>
2019-04-16 15:33:42 +0200

    feat(doubled.ts) add function to decrypt duplicated-chars.txt

---
+++ week-03/day-2/doubled/doubled.ts
@@ -0,0 +1,16 @@
+'use strict';
+const fs = require('fs');
+
+// Create a method that decrypts duplicated-chars.txt
+const content: string = fs.readFileSync('duplicated-chars.txt', 'utf-8');
+
+function doubleDecrypter(path: string): any{
+  let splittedContent: string[] = path.split('');
+  let decoded: string = '';
+  for (let i: number = 0; i < splittedContent.length; i+=2){
+    splittedContent[i] === '.' ? decoded += splittedContent[i] + ' ' : decoded += splittedContent[i];
+  }
+  return decoded;
+}
+
+console.log(doubleDecrypter(content));
