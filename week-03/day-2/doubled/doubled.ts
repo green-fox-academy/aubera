f5df94e756782f674893faefc2be7bb388e8a79f
aubera <andor.auber@gmail.com>
2019-04-16 15:54:07 +0200

    style(doubled.ts) beautify code

--- week-03/day-2/doubled/doubled.ts
+++ week-03/day-2/doubled/doubled.ts
@@ -4,10 +4,10 @@ const fs = require('fs');
 // Create a method that decrypts duplicated-chars.txt
 const content: string = fs.readFileSync('duplicated-chars.txt', 'utf-8');
 
-function doubleDecrypter(path: string): any{
+function doubleDecrypter(path: string): any {
   let splittedContent: string[] = path.split('');
   let decoded: string = '';
-  for (let i: number = 0; i < splittedContent.length; i+=2){
+  for (let i: number = 0; i < splittedContent.length; i += 2) {
     splittedContent[i] === '.' ? decoded += splittedContent[i] + ' ' : decoded += splittedContent[i];
   }
   return decoded;
