a39a245edb718bed1f5c90404f5bcdda1cfb4beb
aubera <andor.auber@gmail.com>
2019-04-17 18:42:01 +0200

    feat(car.ts) add class with constructor to make car instance

---
+++ week-03/day-3/petrol-station/car.ts
@@ -0,0 +1,12 @@
+'use strict';
+import { Station } from './station'
+
+export class Car{
+  gasAmount: number;
+  capacity: number;
+
+  constructor(gasAmount: number = 0, capacity: number = 100){
+    this.gasAmount = gasAmount;
+    this.capacity = capacity;
+  }
+}
