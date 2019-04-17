7db95e51956009324ee5764e890717e4ff2e2402
aubera <andor.auber@gmail.com>
2019-04-17 13:56:49 +0200

    refactor(main.ts) mod function to use inherited .isEffectiveAgainst function

--- week-03/day-3/pokemon/main.ts
+++ week-03/day-3/pokemon/main.ts
@@ -12,27 +12,20 @@ let pokemonOfAsh: Pokemon[] = initializePokemon();
 let wildPokemon: Pokemon = new Pokemon('Oddish', 'leaf', 'water');
 
 // Which pokemon should Ash use?
-function pokemonToChoose(list: Pokemon[]): any{
-  let choice: string = '';
-  for (let i: number = 0; i < list.length; i++){
-    if (wildPokemon.type === list[i].effectiveAgainst){
-      choice = list[i].name;
-    }
-    // if (list[i].isEffectiveAgainst(wildPokemon)){
-    //   return list[i].name;
-    // }
+function pokemonToChoose(list: Pokemon[]): any {
+  if (list[i].isEffectiveAgainst(wildPokemon)) {
+    return list[i].name;
   }
-  return choice;
 }
 
 console.log('I choose you, ' + pokemonToChoose(pokemonOfAsh));
 
 function initializePokemon(): Pokemon[] {
-    return [
-        new Pokemon('Balbasaur', 'leaf', 'water'),
-        new Pokemon('Pikatchu', 'electric', 'water'),
-        new Pokemon('Charizard', 'fire', 'leaf'),
-        new Pokemon('Balbasaur', 'water', 'fire'),
-        new Pokemon('Kingler', 'water', 'fire')
-    ];
+  return [
+    new Pokemon('Balbasaur', 'leaf', 'water'),
+    new Pokemon('Pikatchu', 'electric', 'water'),
+    new Pokemon('Charizard', 'fire', 'leaf'),
+    new Pokemon('Balbasaur', 'water', 'fire'),
+    new Pokemon('Kingler', 'water', 'fire')
+  ];
 }
