da247dc34a0e4e641145b5e833ae75ac190bf5dd
aubera <andor.auber@gmail.com>
2019-04-23 11:41:06 +0200

    feat(cohort.ts) add class Cohort with constructor and methods

---
+++ week-04/day-2/gf-organization/cohort.ts
@@ -0,0 +1,26 @@
+'use strict';
+
+import { Student } from './student'
+import { Mentor } from './mentor'
+
+export class Cohort{
+  name: string;
+  students: Student[] = [];
+  mentors: Mentor[] = [];
+
+  constructor(name: string){
+    this.name = name;
+  }
+
+  addStudent(student: Student): void{
+    this.students.push(student);
+  }
+
+  addMentors(mentor: Mentor): void{
+    this.mentors.push(mentor);
+  }
+
+  info(): void{
+    console.log(`The ${this.name} cohort has ${this.students.length} students and ${this.mentors.length} mentors.`)
+  }
+}
