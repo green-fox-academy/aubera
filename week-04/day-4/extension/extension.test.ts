c0971e16ecb89a3cd737ac794ed9f783cc1c885a
aubera <andor.auber@gmail.com>
2019-04-25 14:06:14 +0200

    feat(extension.test.ts) add 1 case to test median calc for two digit nums

--- week-04/day-4/extension/extension.test.ts
+++ week-04/day-4/extension/extension.test.ts
@@ -44,12 +44,17 @@ test('max of three: second', function(t: any): any {
 });
 
 test('median: four', function(t: any): any {
-  t.equal(median([7, 5, 3, 5]), 5);
+  t.deepEqual(median([7, 5, 3, 5]), [5, 5]);
   t.end();
 });
 
 test('median: five', function(t: any): any {
-  t.equal(median([1, 2, 3, 4, 5]), 3);
+  t.deepEqual(median([1, 2, 3, 4, 5]), [3]);
+  t.end();
+});
+
+test('median: with two digit numbers', function(t: any): any {
+  t.deepEqual(median([3, 5, 5, 7, 9, 10, 11, 12, 13, 14]), [9, 10]);
   t.end();
 });
 
