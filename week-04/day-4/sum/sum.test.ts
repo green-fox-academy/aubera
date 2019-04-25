675e9713ed5344f7c63e3a11b70ac575949f674b
aubera <andor.auber@gmail.com>
2019-04-25 11:49:19 +0200

    feat(sum.test.ts) add 2 test cases for empty list, and list with 1 item

--- week-04/day-4/sum/sum.test.ts
+++ week-04/day-4/sum/sum.test.ts
@@ -9,7 +9,29 @@ test('check getSum() method', (t: any) => {
 
   let actual: number = sum.getSum(arr);
   let expected: number = 15;
-  
+
   t.equal(actual, expected);
   t.end();
 });
+
+test('check getSum() method', (t: any) => {
+  let sum: Sum = new Sum();
+  let arr: number[] = [1];
+
+  let actual: number = sum.getSum(arr);
+  let expected: number = 1;
+
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('check getSum() method', (t: any) => {
+    let sum: Sum = new Sum();
+    let arr: number[] = [];
+
+    let actual: number = sum.getSum(arr);
+    let expected: number = 0
+
+    t.equal(actual, expected);
+    t.end();
+  });
