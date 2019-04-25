e7191f196573de9c08894365cc6ede36c65c6794
aubera <andor.auber@gmail.com>
2019-04-25 12:07:46 +0200

    feat(anagram.ts) add function, return true if two strings are anagrams

---
+++ week-04/day-4/anagram/anagram.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+function isAnagram (a: string, b: string): boolean{
+  let ans: boolean = false;
+  if (a.length === b. length){
+    let bArray: string[] = b.split('');
+    let aArray: string[] = [];
+    for (let i: number = 0; i < a.length; i++){
+      aArray.push(bArray[bArray.indexOf(a[i])]);
+    }
+    b = aArray.join('');
+    a === b ? ans = true : ans;
+  }
+  return ans;
+}
