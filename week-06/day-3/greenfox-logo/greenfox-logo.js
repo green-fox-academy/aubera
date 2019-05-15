650657a1ac33d03d4c12a7bf21e5d1f37c2c454a
aubera <andor.auber@gmail.com>
2019-05-15 15:26:08 +0200

    feat(greenfox-logo.js) add code to change svg properties in HTML

---
+++ week-06/day-3/greenfox-logo/greenfox-logo.js
@@ -0,0 +1,8 @@
+//jshint esversion: 6
+
+// Select left and right eyes
+// They have #left-eye and #right-eye
+// turn their color (fill) to tomato
+
+document.querySelector('#left-eye').setAttribute('fill', 'tomato');
+document.querySelector('#right-eye').setAttribute('fill', 'tomato');
