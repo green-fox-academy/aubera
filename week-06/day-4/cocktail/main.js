a9fbe9e2840340b0d1a221fcb1d8818dd8f5bb0c
aubera <andor.auber@gmail.com>
2019-05-16 12:45:46 +0200

    chore(cocktail) initialize express, main.js, cocktail.ejs

---
+++ week-06/day-4/cocktail/main.js
@@ -0,0 +1,11 @@
+//jshint esversion: 6
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
