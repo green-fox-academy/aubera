de920e2e7e0dfe18f87fc7ab278217c184f8231b
aubera <andor.auber@gmail.com>
2019-05-16 12:38:12 +0200

    feat(index.js) add code to pass todoList to todos.ejs

--- week-06/day-4/todos/index.js
+++ week-06/day-4/todos/index.js
@@ -6,6 +6,13 @@ const PORT = 3000;
 
 app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => {
+  const todos = ['get up', 'survive', 'go back to bed'];
+  res.render('todos', {
+    list: todos
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
