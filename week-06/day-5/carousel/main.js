3d434cd1324df6ec587433d774a25203dfd7665f
aubera <andor.auber@gmail.com>
2019-05-17 16:00:35 +0200

    chore(carousel) project init upload

---
+++ week-06/day-5/carousel/main.js
@@ -0,0 +1,15 @@
+//jshint esversion: 6
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Carousel is listening on port ${PORT}`);
+});
