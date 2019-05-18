57f4704bf70ce2b527ab5b85d8dd7d3814026d2d
aubera <andor.auber@gmail.com>
2019-05-18 19:23:37 +0200

    feat(main.js) add images object list, change get method to render page

--- week-06/day-5/carousel/main.js
+++ week-06/day-5/carousel/main.js
@@ -4,10 +4,23 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const images = [
+  {title: 'Stone fern', src: '/assets/PA112754_1.jpg', description: 'Some nice description'},
+  {title: 'High castle', src: '/assets/PA112556_1.jpg', description: 'Some nice description'},
+  {title: 'Before sunrise', src: '/assets/PA122941_1.jpg', description: 'Some nice description'},
+  {title: '50 shades of grey', src: '/assets/PA112770_1.jpg', description: 'Some nice description'},
+  {title: 'Over the trees', src: '/assets/PA112787_1.jpg', description: 'Some nice description'},
+  {title: 'Look there', src: '/assets/PA112824_1.jpg', description: 'CaptionBot: "I think it\'s a man standing in front of a mountain."'},
+  {title: 'Sunrise', src: '/assets/PA122918_1.jpg', description: 'Some nice description'},
+  {title: 'Water fall', src: '/assets/PA112411_1.jpg', description: 'Some nice description'},
+];
+
 app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
+app.use('/', express.static('./'));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('index', {images: images});
 });
 
 app.listen(PORT, () => {
