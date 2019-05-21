28100db385123bb8dc00e6fbeb19fc813baba2b1
aubera <andor.auber@gmail.com>
2019-05-21 11:42:36 +0200

    feat(callback-1.js) add function to remove every second character in a string

---
+++ week-07/day-2/callback-1.js
@@ -0,0 +1,40 @@
+//jshint esversion:6
+
+const mapWith = (array, callback) => {
+  let output = [];
+  for (let i = 0; i < array.length; i++){
+    output.push(callback(array[i]));
+  }
+
+  // The mapWith() function should iterate over the given array and call the callback function on every element.
+  // It stores the callback return values in the output.
+  // The mapWith() should return with the output array.
+
+  return output;
+};
+
+const addOne = (number) => {
+  return number + 1;
+};
+
+// Exercise 1:
+
+console.log(mapWith([1, 2, 3], addOne));
+//expected result: [2, 3, 4]
+
+// Exercise 2:
+
+// Create a callback function which removes every second character from a string
+
+const words = ['map', 'reduce', 'filter'];
+
+const removeSecondLetter = (item) => {
+  let output = '';
+  for (let i = 0; i < item.length; i+= 2){
+    output += item[i];
+  }
+  return output;
+};
+
+console.log(mapWith(words, removeSecondLetter));
+// expected result: ['mp','rdc', 'fle']
