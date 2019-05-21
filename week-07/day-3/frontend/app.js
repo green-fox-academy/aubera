2a7402486932086b4bf3b29ad498618772a48aa9
aubera <andor.auber@gmail.com>
2019-05-21 22:24:06 +0200

    feat(app.js) init express server

---
+++ week-07/day-3/frontend/app.js
@@ -0,0 +1,13 @@
+//jshint esversion: 6
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('OK!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
