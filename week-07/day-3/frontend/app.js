3ecd50d902b72b4a8aeb0d9c22f166242af12e18
aubera <andor.auber@gmail.com>
2019-05-21 22:34:01 +0200

    feat(app.js) serve index.html upon get req. us estatic folder

--- week-07/day-3/frontend/app.js
+++ week-07/day-3/frontend/app.js
@@ -4,8 +4,10 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.static('assets'));
+
 app.get('/', (req, res) => {
-  res.send('OK!');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(PORT, () => {
