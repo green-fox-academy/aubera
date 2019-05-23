930cd4caf18a8202b1ae04fd3ea8a2d26ff8b950
aubera <andor.auber@gmail.com>
2019-05-23 19:05:37 +0200

    feat(index.js) add filter search action on button click

--- week-07/day-4/bookstore/assets/index.js
+++ week-07/day-4/bookstore/assets/index.js
@@ -1,9 +1,22 @@
 //jshint esversion: 6
 
 const table = document.querySelector('table');
+const button = document.querySelector('button');
+const category = document.querySelector('input[name="category"]');
+const publisher = document.querySelector('input[name="publisher"]');
+const price = document.querySelector('input[name="price"]');
+const greater = document.querySelector('input[name="greater"]');
+const less = document.querySelector('input[name="less"]');
 var bookProperties = ['book_name', 'aut_name', 'cate_descrip', 'pub_name', 'book_price'];
 
 window.onload = ajaxRequest('booktitles');
+button.onclick = () => {
+  while (table.firstChild) {
+    table.removeChild(table.firstChild);
+  }
+  ajaxRequest(generateSearchQuery());
+};
+console.log(table.firstChild);
 
 function ajaxRequest(what) {
   let xhr = new XMLHttpRequest();
@@ -18,11 +31,27 @@ function listBooks(list, names) {
   for (let i = 0; i < list.length; i++) {
     var tr = document.createElement('tr');
     for (let j = 0; j < names.length; j++) {
-      console.log(j);
       var td = document.createElement('td');
       td.textContent = list[i][names[j]];
       tr.appendChild(td);
     }
     table.appendChild(tr);
   }
+}
+
+function generateSearchQuery(){
+  var queryString = 'booktitles?';
+  if (category.value){
+    queryString += 'category=' + category.value;
+  }
+  if (publisher.value){
+    queryString += '&publisher=' + publisher.value;
+  }
+  if (greater.checked){
+    queryString += '&pgt=' + price.value;
+  }
+  if (less.checked){
+    queryString += '&plt=' + price.value;
+  }
+  return queryString;
 }
