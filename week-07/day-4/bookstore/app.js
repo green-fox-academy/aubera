aead07c4186c1efb465c1ac269fdbfed682e71ce
aubera <andor.auber@gmail.com>
2019-05-23 10:39:37 +0200

    feat(app.js) add method to handle get request, add code to connect mysql

--- week-07/day-4/bookstore/app.js
+++ week-07/day-4/bookstore/app.js
@@ -1,7 +1,9 @@
 //jshint esversion: 6
 
+const dotenv = require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
 const app = express();
 const PORT = 3000;
 
@@ -12,6 +14,35 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/booktitles', (req, res) => {
+  connection.query(`SELECT book_name FROM book_mast;`, function(err, rows) {
+    if (err) {
+      console.log(err.toString());
+      res.status(500).send('Database error');
+      return;
+    }
+    console.log('Data received from Db');
+    res.send(rows);
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Bookstore app is listening on port ${PORT}`);
+});
+
+
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: 'bookinfo',
+  port: 3306
+});
+
+connection.connect((error) => {
+  if (error) {
+    console.log('Error connecting to Db', error);
+    return;
+  }
+  console.log('Connection established');
 });
