429273e500899e9dc09f216d398cea4d575ac69c
aubera <andor.auber@gmail.com>
2019-05-23 14:01:36 +0200

    feat(index.js) change code to produce table on HTML

--- week-07/day-4/bookstore/assets/index.js
+++ week-07/day-4/bookstore/assets/index.js
@@ -1,22 +1,28 @@
 //jshint esversion: 6
 
-const booklist = document.querySelector('ul');
+const table = document.querySelector('table');
+var bookProperties = ['book_name', 'aut_name', 'cate_descrip', 'pub_name', 'book_price'];
 
-window.onload(ajaxRequest('booktitles'));
+window.onload = ajaxRequest('booktitles');
 
 function ajaxRequest(what) {
   let xhr = new XMLHttpRequest();
   xhr.open('GET', 'http://localhost:3000/' + what, true);
   xhr.send(null);
   xhr.onload = (data) => {
-    listBooktitles(JSON.parse(data.target.response));
+    listBooks(JSON.parse(data.target.response), bookProperties);
   };
 }
 
-function listBooktitles(list) {
+function listBooks(list, names) {
   for (let i = 0; i < list.length; i++) {
-    var li = document.createElement('li');
-    li.textContent = list[i].book_name;
-    booklist.appendChild(li);
+    var tr = document.createElement('tr');
+    for (let j = 0; j < names.length; j++) {
+      console.log(j);
+      var td = document.createElement('td');
+      td.textContent = list[i][names[j]];
+      tr.appendChild(td);
+    }
+    table.appendChild(tr);
   }
 }
