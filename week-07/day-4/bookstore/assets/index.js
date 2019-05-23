bc1436f8a1570ca14694d09e19ffabd740dd4bc7
aubera <andor.auber@gmail.com>
2019-05-23 10:40:49 +0200

    feat(index.js) add ode to perform ajaxRequest and list book titles

--- week-07/day-4/bookstore/assets/index.js
+++ week-07/day-4/bookstore/assets/index.js
@@ -1,3 +1,22 @@
 //jshint esversion: 6
 
-document.querySelector('h1').style.color = 'red';
+const booklist = document.querySelector('ul');
+
+window.onload(ajaxRequest('booktitles'));
+
+function ajaxRequest(what) {
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', 'http://localhost:3000/' + what, true);
+  xhr.send(null);
+  xhr.onload = (data) => {
+    listBooktitles(JSON.parse(data.target.response));
+  };
+}
+
+function listBooktitles(list) {
+  for (let i = 0; i < list.length; i++) {
+    var li = document.createElement('li');
+    li.textContent = list[i].book_name;
+    booklist.appendChild(li);
+  }
+}
