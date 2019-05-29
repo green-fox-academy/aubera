cd0612d3d6ce8e1142abcdeb1781d042fd429450
aubera <andor.auber@gmail.com>
2019-05-29 14:37:36 +0200

    feat(server.js) add '/signup' endpoint

--- week-08/day-3/newsletter/server.js
+++ week-08/day-3/newsletter/server.js
@@ -9,6 +9,13 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/newsletter.html');
 });
 
+app.post('/signup', (req, res) => {
+  let username = req.body.username;
+  let email = req.body.email;
+  console.log(req.body);
+  res.send(`<h1>Thanks ${username}, we will send updates to ${email}</h1>`);
+});
+
 app.listen(PORT, ()=>{
   console.log(`Newsletter app is listening on port ${PORT}`);
 });
