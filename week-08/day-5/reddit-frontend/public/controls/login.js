9f38a9fbb27355aa93c9a13f40e325e38e14c666
aubera <andor.auber@gmail.com>
2019-06-01 19:08:21 +0200

    feat(login.js) add button.onclick function to submit button

---
+++ week-08/day-5/reddit-frontend/public/controls/login.js
@@ -0,0 +1,26 @@
+const button = document.getElementsByTagName('button')[0];
+const username = document.querySelectorAll('input')[0];
+const password = document.querySelectorAll('input')[1];
+
+
+
+button.onclick = (event)=> {
+  event.preventDefault();
+  checkLoginData();
+};
+
+function checkLoginData() {
+  let xhr = new XMLHttpRequest();
+  xhr.open('POST', 'http://localhost:3000/login', true);
+  xhr.setRequestHeader('Accept', 'application/json');
+  xhr.setRequestHeader('username', username.value);
+  xhr.setRequestHeader('password', password.value);
+  xhr.send();
+  xhr.onload = (data) => {
+    console.log(xhr.status);
+    sessionStorage.setItem('redditUserName', JSON.parse(data.target.response).username);
+    window.location.href = 'http://localhost:3000/';
+  };
+}
+
+
