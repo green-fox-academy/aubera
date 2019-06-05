aab2918d2f431447dd1dd45c3923bb4114f300c4
aubera <andor.auber@gmail.com>
2019-06-05 16:12:07 +0200

    feat(routes.js) add get rocket/ and get rocket/fill endpoints

--- week-09/day-3/iamgroot/routes.js
+++ week-09/day-3/iamgroot/routes.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 
+let ship = {
+  "caliber25": 0,
+  "caliber30": 0,
+  "caliber50": 0,
+  "shipstatus": "empty",
+  "ready": false
+};
+let maxAmmo = 12500;
+
 app.get('/groot/', (req, res) => {
   let translate = {};
   if (req.query.message != undefined){
@@ -27,6 +36,53 @@ app.get('/yondu/', (req, res) => {
   } else {
     calc.error = 'Give me data!';
     res.status(400).json(calc);
+  }
+});
+
+app.get('/rocket/', (req, res) => {
+  res.status(200).json(ship);
+});
+
+app.get('/rocket/fill', (req, res) => {
+  if (req.query.caliber != undefined && req.query.amount != undefined){
+    let amountToFill = Number(req.query.amount);
+    let actualAmmo;
+    if (ship.shipstatus === 'empty'){
+      actualAmmo = 0;
+    } else if (ship.shipstatus != 'full' || ship.shipstatus != 'overloaded'){
+      actualAmmo = (Number(ship.shipstatus.substring(0,2)) / 100) * maxAmmo;
+    } else if (ship.shipstatus === 'full' || ship.shipstatus != 'overloaded'){
+      actualAmmo = maxAmmo;
+    }
+
+    actualAmmo += amountToFill;
+    let fillPercent = Math.round((actualAmmo / maxAmmo) * 100);
+    if (fillPercent <= 0){
+      ship.shipstatus = 'empty';
+    } else if (fillPercent < 100){
+      ship.shipstatus = fillPercent + '%';
+    } else if (fillPercent === 100){
+      ship.shipstatus = 'full';
+      ship.ready = true;
+    } else {
+      ship.shipstatus = 'overloaded';
+      ship.ready = false;
+    }
+
+    if (req.query.caliber === '.50'){
+      ship.caliber50 = Number(ship.caliber50) + amountToFill;
+    }
+    if (req.query.caliber === '.30'){
+      ship.caliber30 = Number(ship.caliber30) + amountToFill;
+    }
+    if (req.query.caliber === '.25'){
+      ship.caliber25 = Number(ship.caliber25) + amountToFill;
+    }
+    res.status(200).json(ship);
+  } else {
+    let err = {};
+    err.message = 'Give me valid data!';
+    res.status(400).json(err);
   }
 });
 
